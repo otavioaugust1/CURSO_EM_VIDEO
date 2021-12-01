03ee2786c9bb179d4696c4a2b5cb4a97099e1d93
Otavio Augusto <otavioaugust@gmail.com>
2021-12-01 17:45:26 -0300

    01-12-2021
    17h45 subindo material de apoio

---
+++ aula_python3/Mundo_1/ex_000.py
@@ -0,0 +1,9 @@
+nome = 'Otavio'
+idade = 38
+peso = 87.9
+print (nome, idade, peso) 
+
+nome = input('Qual é seu nome?')
+idade = input('Qual é sua idade?')
+peso  = input('Qual é seu peso?')
+print (nome, idade, peso)
