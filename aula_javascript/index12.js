ad18d57643f3f2b53dd2693af6269bea5af97e67
Otavio Augusto <otavioaugust@gmail.com>
2021-12-10 18:49:46 -0300

    10-12-2021
    18h50 aula 12 JS

--- aula_javascript/index12.js
+++ aula_javascript/index12.js
@@ -1,9 +1,10 @@
-var idade = 67
-console.log (`Você tem ${idade} anos.`)
-if (idade < 16 ){
-    console.log ('Não vota')
-} else if (idade < 18 || idade >65 ){
-    console.log ('Voto opcional')
+var agora = new Date()
+var hora = agora.getHours()
+console.log (`Agora são exatamente ${hora} horas.`)
+if (hora < 12){
+    console.log ('Bom dia!')
+} else if (hora <=18) {
+    console.log ('Boa tarde!')    
 } else {
-    console.log ('Voto obrigatório')
+    console.log ('Boa noite!')
 }
---
+++ aula_javascript/index13.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+console.log (diaSem)
+switch (diaSem){
+    case 0:
+        console.log ('DOMINGO')
+        break
+    case 1:
+        console.log ('SEGUNDA')
+        break
+    case 2:
+        console.log ('TERÇA')
+        break
+    case 3:
+        console.log ('QUARTA')
+        break
+    case 4:
+        console.log ('QUINTA')
+        break
+    case 5:
+        console.log ('SEXTA')
+        break
+    case 6:
+        console.log ('SABADO')
+        break
+}
