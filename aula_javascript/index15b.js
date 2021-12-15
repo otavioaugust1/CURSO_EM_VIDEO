50aaa63480ced2317754d1de0ce2af24f27adefb
Otavio Augusto <otavioaugust@gmail.com>
2021-12-15 18:40:26 -0300

    15-12-2021
    18h40 aula ex 2 JS

--- aula_javascript/index15b.js
+++ aula_javascript/index15b.js
@@ -5,11 +5,11 @@ function carregar(){
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora > 0 && hora < 12){ // BOM DIA
-        img.src = 
+        img.src = 'fotoManha.png'
     } else if (hora >= 12 && hora < 18) { //BOA TARDE
-
+        img.src = 'fotoTarde.png'
     } else { //BOA NOITE
-
+        img.src = 'fotoNoite.png'
     }
 }
 
