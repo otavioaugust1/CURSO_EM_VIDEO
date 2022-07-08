522b56a672676836af6a610f2229d73f1e9772c3
Otavio Augusto <otavioaugust@gmail.com>
2022-07-08 18:05:23 -0300

    08-07-2022

--- aula_python3/Mundo_1/ex_003.py
+++ aula_python3/Mundo_1/ex_003.py
@@ -1,4 +1,9 @@
 n1 = int(input('Digite um número '))
 n2 = int(input('Digite mais um número '))
 s = n1+n2
-print('A soma vale', s)
+# print('A soma vale', s)  padrão antigo do python 2
+
+print('A soma entre {} e {} vale {}}'.format(n1, n2, s))
+
+
+#  int() float() bool() e str()  tipos primitivos....
